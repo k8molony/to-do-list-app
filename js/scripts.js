4df9a27c6b9a936e526276514fc256e9157ea58f
Katy Molony <k8molony@att.net>
2021-08-05 12:17:54 -0400

    Added jQuery code to make to-do list

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function newItem(){
+
+  //Add a new item to the list of items:
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    let list = $('#list');
+    list.append(li);
+  }
+
+  //Cross out an item from the list
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+  //Add a delete button to the list items
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+  function deleteListItem(){
+    li.addClass('delete')
+  }
+
+  //Make list sortable
+  $('#list').sortable();
+}
